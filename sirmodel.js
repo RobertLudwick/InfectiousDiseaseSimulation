353c0116d43e13dbb05a7eb76d9b48493c57345d
Robert Ludwick <rludwick@drew.edu>
2018-10-26 10:30:34 -0400

    Added functionality for the clear button and the beginnings of the table in sirmodel.js

--- sirmodel.js
+++ sirmodel.js
@@ -76,8 +76,30 @@ function Autorun() {
     var duration = document.getElementById("infectionDuration").value;
     var transmission = document.getElementById("TransRate").value;
    	var data = new model(imunity, virulence, duration, transmission)
-   	var out = document.getElementById("myTable");
-   	document.getElementById("demo").innerHTML = transmission;
-   			
+   	//document.getElementById("demo").innerHTML = transmission;
+   	Table(data)
 	//document.getElementById("demo").innerHTML = transmission;
 	}
+	
+function Table(data) {
+    var x = document.createElement("TABLE");
+    x.setAttribute("id", "myTable");
+    document.getElementById("tableOut").appendChild(x);
+	for (i = 0; i < 30; i++) {
+    var y = document.createElement("TR");
+    y.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(y);
+
+    var z = document.createElement("TD");
+    var t = document.createTextNode("cell");
+    z.appendChild(t);
+    document.getElementById("myTr").appendChild(z);
+    }
+}
+
+function Clear(data) {
+	document.getElementById("initialImmunity").value = "";
+	document.getElementById("Virulence").value = "";
+	document.getElementById("infectionDuration").value = "";
+	document.getElementById("TransRate").value = "";
+}
