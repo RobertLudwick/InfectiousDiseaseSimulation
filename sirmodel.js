7b40b4e3ba58a3f78be58a75e18ca50b0ee519d3
Kibugi Kamau Mbugua <mbg.kamau@gmail.com>
2018-10-31 11:17:01 -0400

    js for index.html in index.js, update to model

--- sirmodel.js
+++ sirmodel.js
@@ -1,6 +1,6 @@
 
 
-function sirmodel(initial_population, infection_duration, Rzero, percent_die, percent_immune, initial_infected) {
+function sirmodel(initial_population, infection_duration, trans_rate, percent_die, percent_immune, initial_infected) {
 
     // fixed parameters
     var sim_period = 30
@@ -25,19 +25,19 @@ function sirmodel(initial_population, infection_duration, Rzero, percent_die, pe
         dead.push(0)
 
         for(n=1; n <= sim_period; n++) {
-            s_prime = s[n-1] - (Rzero * s[n-1] * i[n-1]/initial_population)
-            i_prime = i[n-1] + (Rzero * s[n-1] * i[n-1]/initial_population - i[n-1]/infection_duration - i[n-1] * percent_die/(100*infection_duration))
+            s_prime = s[n-1] - (trans_rate * s[n-1] * i[n-1]/initial_population)
+            i_prime = i[n-1] + (trans_rate * s[n-1] * i[n-1]/initial_population - i[n-1]/infection_duration - i[n-1] * percent_die/(100*infection_duration))
             r_prime = r[n-1] + (i[n-1]/infection_duration)
             dead_prime = percent_die * i[n-1]/(100 * infection_duration) + dead[n-1]
             N_prime = s_prime + i_prime + r_prime
 
             
             days.push(n)
-            s.push(Math.round(s_prime)) 
-            i.push(Math.round(i_prime))
-            r.push(Math.round(r_prime))
-            dead.push(Math.round(dead_prime))
-            N.push(Math.round(N_prime))
+            s.push(round(s_prime, 2)) 
+            i.push(round(i_prime, 2))
+            r.push(round(r_prime, 2))
+            dead.push(round(dead_prime, 2))
+            N.push(round(N_prime, 2))
         }
 
     }
@@ -56,117 +56,20 @@ function sirmodel(initial_population, infection_duration, Rzero, percent_die, pe
 
 // use this to get your number, it's like temporary interface to get numbers
 // it can return the list of number we see on the website. all values in percent
-function model(percent_immune, virulence, infection_duration, transmission_rate){
-    // default values
+function model(percent_immune, virulence, infection_duration, trans_rate){
+    // default/fixed values
     var initial_population = 100000
-    var Rzero = 1
     var initial_infected = 1
 
-    var model = new sirmodel(initial_population, infection_duration, Rzero, virulence, percent_immune, initial_infected)
+    var model = new sirmodel(initial_population, infection_duration, trans_rate, virulence, percent_immune, initial_infected)
     this.days = model.days
     this.susceptible = model.Susceptible
     this.sick = model.infected
-    this.immune = model.immune + model.dead
+    this.immune = model.immune
     this.population = model.population
 }
 
-function Autorun() {
-    var imunity = document.getElementById("initialImmunity").value;
-    var virulence = document.getElementById("Virulence").value;
-    var duration = document.getElementById("infectionDuration").value;
-    var transmission = document.getElementById("TransRate").value;
-   	var data = new model(imunity, virulence, duration, transmission)
-   	//document.getElementById("demo").innerHTML = transmission;
-   	Table(data)
-	//document.getElementById("demo").innerHTML = transmission;
-	}
-	
-function Table(data) {
-    var x = document.createElement("TABLE");
-    x.setAttribute("id", "myTable");
-    document.getElementById("tableOut").appendChild(x);
-	for (i = 0; i < 30; i++) {
-    var y = document.createElement("TR");
-    y.setAttribute("id", "myTr");
-    document.getElementById("myTable").appendChild(y);
-
-    var z = document.createElement("TD");
-    var t = document.createTextNode("cell");
-    z.appendChild(t);
-    document.getElementById("myTr").appendChild(z);
-    }
-}
-
-function Clear(data) {
-	document.getElementById("initialImmunity").value = "";
-	document.getElementById("Virulence").value = "";
-	document.getElementById("infectionDuration").value = "";
-	document.getElementById("TransRate").value = "";
-}
-
-function gatherData() {
-	var imunity = document.getElementById("initialImmunity").value;
-    var virulence = document.getElementById("Virulence").value;
-	var duration = document.getElementById("infectionDuration").value;
-    var transmission = document.getElementById("TransRate").value;
-	if (imunity > 100 || imunity < 0) {
-		document.getElementById("demo").innerHTML = "Error: incorrect imunity value entered";
-	}
-	if (virulence > 0.75 || imunity < 0) {
-		document.getElementById("demo").innerHTML = "Error: incorrect virulence value entered";
-	}
-	if (duration > 20 || duration < 1) {
-		document.getElementById("demo").innerHTML = "Error: incorrect Duration of infection value entered";
-	}
-	if (transmission > 10.0 || transmission < 0.1) {
-		document.getElementById("demo").innerHTML = "Error: incorrect Rate of Transmission value entered";
-	}
-	
-
-}
-
- function generateData() {
-                gatherData()
-                var imunity = document.getElementById("initialImmunity").value;
-    			var virulence = document.getElementById("Virulence").value;
-    			var duration = document.getElementById("infectionDuration").value;
-    			var transmission = document.getElementById("TransRate").value;
-                var data = new model(imunity, virulence, duration, transmission)
-                var table  = document.getElementById("myTable")
-                for (day in data.days) {
-                    var row = table.insertRow(-1);
-
-                    var cell1 = row.insertCell(0)
-                    var cell2 = row.insertCell(1)
-                    var cell3 = row.insertCell(2)
-                    var cell4 = row.insertCell(3)
-
-                    cell1.innerHTML = data.days[day]
-                    cell2.innerHTML = data.sick[day]
-                    cell3.innerHTML = data.immune[day]
-                    cell4.innerHTML = data.population[day]
-                }
-            }
-
-function generateDatadaybyday() {
-                gatherData()
-                var imunity = document.getElementById("initialImmunity").value;
-    			var virulence = document.getElementById("Virulence").value;
-    			var duration = document.getElementById("infectionDuration").value;
-    			var transmission = document.getElementById("TransRate").value;
-                var data = new model(imunity, virulence, duration, transmission)
-                var table  = document.getElementById("myTable")
-                for (day in data.days) {
-                    var row = table.insertRow(-1);
-
-                    var cell1 = row.insertCell(0)
-                    var cell2 = row.insertCell(1)
-                    var cell3 = row.insertCell(2)
-                    var cell4 = row.insertCell(3)
-
-                    cell1.innerHTML = data.days[day]
-                    cell2.innerHTML = data.sick[day]
-                    cell3.innerHTML = data.immune[day]
-                    cell4.innerHTML = data.population[day]
-                }
-            }
+// helper functions
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
