b4bcfc85a4aa61410db3e4f81b5614b93c4d022b
Kibugi Kamau Mbugua <mbg.kamau@gmail.com>
2018-11-05 08:50:29 -0500

    checkbox for graph

--- graph.js
+++ graph.js
@@ -1,15 +1,14 @@
 
-susceptible_cb = document.getElementById("susceptible_cb")
-infected_cb = document.getElementById("infected_cb")
-immune_cb = document.getElementById("immune_cb")
-dead_cb = document.getElementById("dead_cb")
-population_cb = document.getElementById("population_cb")
+// var susceptible_cb = document.getElementById("susceptible_cb")
+// var infected_cb = document.getElementById("infected_cb")
+// var immune_cb = document.getElementById("immune_cb")
+// var dead_cb = document.getElementById("dead_cb")
+// var population_cb = document.getElementById("population_cb")
+var data
 
-function  run() {
- 
-
+function  build_graph(data) {
+    console.log("built")
     var d = document.getElementById('tester')
-    var data = new model(0, 5, 2, 1)
     var type_a = "line"
     var line_a = {shape: "spline"}
 
@@ -18,8 +17,9 @@ function  run() {
         y: data.susceptible,
         type: type_a,
         line: line_a,
-        name: "susceptible",
-        visible: susceptible_cb.checked
+        visible: document.getElementById("susceptible_cb").checked,
+        name: "susceptible"
+        
         
     };
     var infected_trace = {
@@ -27,6 +27,7 @@ function  run() {
         y: data.infected,
         type: type_a,
         line: line_a,
+        visible: document.getElementById("infected_cb").checked,
         name: "infected"
     };
     var immune_trace = {
@@ -34,6 +35,7 @@ function  run() {
         y: data.immune,
         type: type_a,
         line: line_a,
+        visible: document.getElementById("immune_cb").checked,
         name: "immune"
     };
     var dead_trace = {
@@ -41,6 +43,7 @@ function  run() {
         y: data.dead,
         type: type_a,
         line: line_a,
+        visible: document.getElementById("dead_cb").checked,
         name: "dead"
     };
     var population_trace = {
@@ -48,6 +51,7 @@ function  run() {
         y: data.population,
         type: type_a,
         line: line_a,
+        visible: document.getElementById("population_cb").checked,
         name: "population"
     };
     
@@ -63,4 +67,13 @@ function  run() {
     }
 
     Plotly.newPlot(d, to_plot, layout)
+}
+
+function show() {
+    build_graph(data)
+}
+
+function run() {
+    data = new model(0, 5, 2, 1)
+    build_graph(data)
 }
